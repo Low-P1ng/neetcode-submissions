e6f0eae6f862f7943b484ebd8c7542c32119d024
Surya Namdev <87803299+Low-P1ng@users.noreply.github.com>
2026-06-29 13:11:34 +0530

    Add: partition-labels - submission-0

---
+++ Data Structures & Algorithms/partition-labels/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        idx = defaultdict(list)
+        res = []
+        for i,l in enumerate(s):
+            idx[l].append(i)
+        startidx = 0
+        endidx = idx[s[0]][-1]
+        for l in idx.keys():
+            if idx[l][0] < endidx:
+                endidx = max(endidx, idx[l][-1])
+                continue
+            
+            if idx[l][0] > endidx:
+                res.append(endidx - startidx + 1)
+                startidx = idx[l][0]
+                endidx = idx[l][-1]
+        res.append(endidx - startidx + 1)
+        return res
